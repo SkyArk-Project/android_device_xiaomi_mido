e86821036f4030d5d51f669fa4ae341928415755
kondors1995 <normandija1945@gmail.com>
2018-12-12 20:25:54 +0000

    Build OTA & include HWIUI 3gig config

--- device.mk
+++ device.mk
@@ -396,3 +396,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+
+# HWUI and Dalvik VM overrides
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+
+# OTA Updates
+PRODUCT_PACKAGES += \
+	Updates
